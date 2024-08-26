5926298e5c9f26eefa12145ff4c894af20b1097f
Rafael <rafagpoliveira@gmail.com>
2024-08-26 19:20:27 -0300

    fix gitignore

--- src/infra/repositories/InMemoryDriverRepository.ts
+++ src/infra/repositories/InMemoryDriverRepository.ts
@@ -1,10 +1,19 @@
 import { Driver } from '../../domain/entities/Driver';
 import { IDriverRepository } from '../../domain/interfaces/IDriverRepository';
+import { DriverEntity } from '../entities/DriverEntity';
 
 export class InMemoryDriverRepository implements IDriverRepository {
   private drivers: Driver[] = [];
 
   async createDriver(driver: Driver): Promise<void> {
     this.drivers.push(driver);
+  }
+
+  async findAll(): Promise<any> {
+    
+  }
+
+  async findByCpf(cpf: string): Promise<any> {
+    
   }
 }
